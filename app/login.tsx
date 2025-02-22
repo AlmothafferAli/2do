bf3d11f80160c40ac5a45ba95f8b35d212a7008a
Lucifer <almothafferali@gmail.com>
2025-02-22 19:24:28 +0300

    sec

--- app/login.tsx
+++
@@ -1,9 +0,0 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
-export default function Login() {
-  return (
-    <TouchableOpacity onPress={() => router.replace("./index")}>
-      <Text>login</Text>
-    </TouchableOpacity>
-  );
-}
